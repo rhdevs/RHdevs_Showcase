8c318a29a2a1bc5b39f24a736d4a55f0fa70179d
chuahziyang <czyang2002@gmail.com>
2024-02-05 00:11:01 +0800

    added tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { Routes } from "@generouted/react-router";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import generouted from "@generouted/react-router/plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), generouted()],
+});
