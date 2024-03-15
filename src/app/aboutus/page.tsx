8342f0428ec672af7de7195c07e354ec4a044613
chuahziyang <czyang2002@gmail.com>
2024-03-15 17:13:39 +0800

    added pics

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -61,7 +61,10 @@ const Person = ({ name }: PersonProps) => {
         className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8"
       >
         {people.map((person) => (
-          <li key={person.name} className="rounded-2xl bg-gray-800 px-8 py-10">
+          <li
+            key={person.name}
+            className="rounded-2xl bg-black border-2 border-gray-800 px-8 py-10"
+          >
             <img
               className="mx-auto h-48 w-48 rounded-full md:h-56 md:w-56"
               src={person.imageUrl}
@@ -74,22 +77,6 @@ const Person = ({ name }: PersonProps) => {
               {person.role}
             </p>
             <ul role="list" className="mt-6 flex justify-center gap-x-6">
-              <li>
-                <a
-                  href={person.twitterUrl}
-                  className="text-gray-400 hover:text-gray-300"
-                >
-                  <span className="sr-only">Twitter</span>
-                  <svg
-                    className="h-5 w-5"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                  </svg>
-                </a>
-              </li>
               <li>
                 <a
                   href={person.instagramUrl}
@@ -119,36 +106,29 @@ interface Person {
   name: string;
   role: string;
   imageUrl: string;
-  twitterUrl: string;
   linkedinUrl: string;
   instagramUrl: string;
 }
 
 const people: Person[] = [
   {
-    name: "Chuah Zi Yang",
-    role: "Senior Designer",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-    twitterUrl: "#",
+    name: "Low Wei Sheng",
+    role: "Frontend Head",
+    imageUrl: "weisheng.jpg",
     linkedinUrl: "#",
     instagramUrl: "#",
   },
   {
-    name: "Xin Min",
-    role: "Senior Designer",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-    twitterUrl: "#",
+    name: "Kwang Yang",
+    role: "RHDevs Head",
+    imageUrl: "ky.jpg",
     linkedinUrl: "#",
     instagramUrl: "#",
   },
   {
-    name: "Farhan Navas",
-    role: "Senior Designer",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-    twitterUrl: "#",
+    name: "Manoj Mahender",
+    role: "Backend Head",
+    imageUrl: "manoj.jpg",
     linkedinUrl: "#",
     instagramUrl: "#",
   },
--- src/app/projects/[project]/page.tsx
+++ src/app/projects/[project]/page.tsx
@@ -1,65 +1,165 @@
 import Wrapper from "~/app/_components/wrapper";
 
-const project = {
-  name: "Project",
-  description:
-    "MTRAC+ is an all in one platform built for Transport Operators (Drivers). Platform will allow TO's and Commanders to manage administrative procedures directly on the platform.",
-  stack: [
-    {
-      name: "React",
-      color: "blue",
-    },
-    {
-      name: "Node",
-      color: "green",
-    },
-    {
-      name: "Express",
-      color: "blue",
-    },
-    {
-      name: "MongoDB",
-      color: "gray",
-    },
-  ],
-  stats: [
-    {
-      name: "Transport Operators Served",
-      description: "30",
-    },
-    {
-      name: "Impact",
-      description: "50%",
-    },
-    {
-      name: "Impact",
-      description: "21 Times",
-    },
-  ],
-  actions: [
-    {
-      title: "Digital MTRAC Forms",
-      description:
-        "lorem ipsum dolor sit amet, consectetur adipisicing elit 1 Description 1",
-    },
-    {
-      title: "Digital MTRAC Forms",
-      description:
-        "lorem ipsum dolor sit amet, consectetur adipisicing elit 1 Description 1",
-    },
-    {
-      title: "Digital MTRAC Forms",
-      description:
-        "lorem ipsum dolor sit amet, consectetur adipisicing elit 1 Description 1",
-    },
-    {
-      title: "Action 2",
-      description: "Description 2",
-    },
-  ],
+const allprojects: Project[] = [
+  {
+    id: 1,
+    name: "Project",
+    description:
+      "MTRAC+ is an all in one platform built for Transport Operators (Drivers). Platform will allow TO's and Commanders to manage administrative procedures directly on the platform.",
+    stack: [
+      {
+        name: "React",
+        color: "blue",
+      },
+      {
+        name: "Node",
+        color: "green",
+      },
+      {
+        name: "Express",
+        color: "blue",
+      },
+      {
+        name: "MongoDB",
+        color: "gray",
+      },
+    ],
+    stats: [
+      {
+        name: "Transport Operators Served",
+        description: "30",
+      },
+      {
+        name: "Impact",
+        description: "50%",
+      },
+      {
+        name: "Impact",
+        description: "21 Times",
+      },
+    ],
+    actions: [
+      {
+        title: "Digital MTRAC Forms",
+        description:
+          "lorem ipsum dolor sit amet, consectetur adipisicing elit 1 Description 1",
+      },
+      {
+        title: "Digital MTRAC Forms",
+        description:
+          "lorem ipsum dolor sit amet, consectetur adipisicing elit 1 Description 1",
+      },
+      {
+        title: "Digital MTRAC Forms",
+        description:
+          "lorem ipsum dolor sit amet, consectetur adipisicing elit 1 Description 1",
+      },
+      {
+        title: "Action 2",
+        description: "Description 2",
+      },
+    ],
+  },
+];
+
+type Project = {
+  id: number;
+  name: string;
+  description: string;
+  stack: {
+    name: string;
+    color: string;
+  }[];
+  stats: {
+    name: string;
+    description: string;
+  }[];
+  actions: {
+    title: string;
+    description: string;
+  }[];
 };
 
 export default function Page({ params }: { params: { project: string } }) {
+  const project = allprojects.find(
+    (project) => project.id === Number(params.project),
+  );
+
+  if (!project) {
+    return <div>Project not found</div>;
+  }
+
+  const Stack = () => {
+    return (
+      <div className="mb-2 flex flex-wrap">
+        {project.stack.map((stack) => (
+          <span
+            key={stack.name}
+            className={`mr-2 mb-2 rounded-sm px-2 py-1 text-xs font-medium text-white bg-${stack.color}-600`}
+          >
+            {stack.name}
+            test
+          </span>
+        ))}
+      </div>
+    );
+  };
+
+  const Impact = () => {
+    return (
+      <div>
+        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          {project.stats.map((item) => (
+            <div
+              key={item.name}
+              className="px-4 py-5 bg-black shadow rounded-lg overflow-hidden border-2 dark:border-gray-800 sm:p-6"
+            >
+              <dt className="text-sm font-medium text-gray-500 truncate">
+                {item.name}
+              </dt>
+              <dd className="mt-1 text-3xl font-semibold text-gray-900 dark:text-gray-200">
+                {item.description}
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    );
+  };
+  const Features: React.FC = () => {
+    return (
+      <div className="rounded-lg  bg-black border-gray-800 border-2 overflow-hidden shadow divide-y divide-gray-800 dark:divide-gray-800 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
+        {project.actions.map((action, actionIdx) => (
+          <div
+            key={action.title}
+            className={`${
+              actionIdx === 0
+                ? "rounded-tl-lg rounded-tr-lg sm:rounded-tr-none"
+                : ""
+            } ${actionIdx === 1 ? "sm:rounded-tr-lg" : ""} ${
+              actionIdx === project.actions.length - 2 ? "sm:rounded-bl-lg" : ""
+            } ${
+              actionIdx === project.actions.length - 1
+                ? "rounded-bl-lg rounded-br-lg sm:rounded-bl-none"
+                : ""
+            } relative group bg:black p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500`}
+          >
+            <div>
+              <h3 className="text-lg font-medium">
+                <button className="focus:outline-none">
+                  {/* Extend touch target to entire panel */}
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  {action.title}
+                </button>
+              </h3>
+              <p className="mt-2 text-sm text-gray-500">{action.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <Wrapper>
       <div className="mx-28 mt-16">
@@ -74,79 +174,6 @@ export default function Page({ params }: { params: { project: string } }) {
     </Wrapper>
   );
 }
-
-const Stack = () => {
-  return (
-    <div className="mb-2 flex flex-wrap">
-      {project.stack.map((stack) => (
-        <span
-          key={stack.name}
-          className={`mr-2 mb-2 rounded-sm px-2 py-1 text-xs font-medium text-white bg-${stack.color}-600`}
-        >
-          {stack.name}
-          test
-        </span>
-      ))}
-    </div>
-  );
-};
-
-const Impact = () => {
-  return (
-    <div>
-      <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-        {project.stats.map((item) => (
-          <div
-            key={item.name}
-            className="px-4 py-5 bg-black shadow rounded-lg overflow-hidden border-2 dark:border-gray-800 sm:p-6"
-          >
-            <dt className="text-sm font-medium text-gray-500 truncate">
-              {item.name}
-            </dt>
-            <dd className="mt-1 text-3xl font-semibold text-gray-900 dark:text-gray-200">
-              {item.description}
-            </dd>
-          </div>
-        ))}
-      </dl>
-    </div>
-  );
-};
-
-const Features: React.FC = () => {
-  return (
-    <div className="rounded-lg  bg-black border-gray-800 border-2 overflow-hidden shadow divide-y divide-gray-800 dark:divide-gray-800 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
-      {project.actions.map((action, actionIdx) => (
-        <div
-          key={action.title}
-          className={`${
-            actionIdx === 0
-              ? "rounded-tl-lg rounded-tr-lg sm:rounded-tr-none"
-              : ""
-          } ${actionIdx === 1 ? "sm:rounded-tr-lg" : ""} ${
-            actionIdx === project.actions.length - 2 ? "sm:rounded-bl-lg" : ""
-          } ${
-            actionIdx === project.actions.length - 1
-              ? "rounded-bl-lg rounded-br-lg sm:rounded-bl-none"
-              : ""
-          } relative group bg:black p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500`}
-        >
-          <div>
-            <h3 className="text-lg font-medium">
-              <button href={1 + 1} className="focus:outline-none">
-                {/* Extend touch target to entire panel */}
-                <span className="absolute inset-0" aria-hidden="true" />
-                {action.title}
-              </button>
-            </h3>
-            <p className="mt-2 text-sm text-gray-500">{action.description}</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
 // For Tailwind Compiler
 // bg-purple-600
 // bg-blue-600
