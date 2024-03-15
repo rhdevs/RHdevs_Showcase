a25071037eb6756532b1935e56985da6061e7b9e
farhan-navas <farhanmnavas@gmail.com>
2024-03-16 01:37:32 +0800

    Added icons fixed resources

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,15 @@ import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
 
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+library.add(fab, fas, far);
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,19 +1,23 @@
 import Wrapper from "../_components/wrapper";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const frontend = [
   {
     title: "Intro to React",
-    image: "file.jpeg",
-    link: "/file.png",
+    prefix: "fab",
+    icon: "react",
+    link: "/intro-to-react.pptx",
   },
   {
     title: "Intro to Redux",
-    image: "file.jpeg",
+    prefix: "fas",
+    icon: "laptop-code",
     link: "/intro-to-redux.pptx",
   },
   {
     title: "JavaScript & TypeScript Basics",
-    image: "file.jpeg",
+    prefix: "fab",
+    icon: "square-js",
     link: "/intro-to-ts-js.pptx",
   },
 ];
@@ -21,17 +25,20 @@ const frontend = [
 const backend = [
   {
     title: "Python Basics",
-    image: "file.jpeg",
+    prefix: "fab",
+    icon: "python",
     link: "/python.pptx",
   },
   {
     title: "Flask & Web Servers",
-    image: "file.jpeg",
+    prefix: "fas",
+    icon: "flask",
     link: "/flask.pptx",
   },
   {
     title: "Database Basics",
-    image: "file.jpeg",
+    prefix: "fas",
+    icon: "database",
     link: "/db.pptx",
   },
 ];
@@ -39,17 +46,20 @@ const backend = [
 const design = [
   {
     title: "Design 101",
-    image: "file.jpeg",
+    prefix: "fab",
+    icon: "figma",
     link: "/design.pdf",
   },
   {
     title: "Typography",
-    image: "file.jpeg",
+    prefix: "fas",
+    icon: "font",
     link: "/typography.pdf",
   },
   {
     title: "Composition",
-    image: "file.jpeg",
+    prefix: "fas",
+    icon: "pen-nib",
     link: "/composition.pdf",
   },
 ];
@@ -65,31 +75,24 @@ export default function Example() {
           things full-stack development. Click on any of the links below to
           download the slides for your own reading!
         </h4>
-        <div className="mt-16 w-full  flex-col flex">
+        <div className="mt-10 w-full  flex-col flex">
           <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Frontend
           </p>
           <ul
             role="list"
-            className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8"
+            className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8 mt-8"
           >
             {frontend.map((file) => (
-              <li key={file.title} className="relative">
-                <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-                  {/* <img
-                    src={file.image}
-                    alt=""
-                    className="pointer-events-none object-cover group-hover:opacity-75"
-                  /> */}
-                  {/* <button
-                    type="button"
-                    className="absolute inset-0 focus:outline-none"
-                  >
-                    <span className="sr-only">
-                      View details for {file.title}
-                    </span>
-                  </button> */}
-                </div>
+              <li
+                key={file.title}
+                className="relative flex flex-col justify-center items-center"
+              >
+                <FontAwesomeIcon
+                  icon={[file.prefix, file.icon]}
+                  size="3x"
+                  color="white"
+                />
                 <a
                   href={file.link}
                   download={file.title}
@@ -101,31 +104,24 @@ export default function Example() {
             ))}
           </ul>
         </div>
-        <div className="mt-16 w-full  flex-col flex">
+        <div className="mt-10 w-full  flex-col flex">
           <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Backend
           </p>
           <ul
             role="list"
-            className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8"
+            className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8 mt-8"
           >
             {backend.map((file) => (
-              <li key={file.title} className="relative">
-                <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-                  {/* <img
-                    src={file.image}
-                    alt=""
-                    className="pointer-events-none object-cover group-hover:opacity-75"
-                  /> */}
-                  {/* <button
-                    type="button"
-                    className="absolute inset-0 focus:outline-none"
-                  >
-                    <span className="sr-only">
-                      View details for {file.title}
-                    </span>
-                  </button> */}
-                </div>
+              <li
+                key={file.title}
+                className="relative flex flex-col justify-center items-center"
+              >
+                <FontAwesomeIcon
+                  icon={[file.prefix, file.icon]}
+                  size="3x"
+                  color="white"
+                />
                 <a
                   href={file.link}
                   download={file.title}
@@ -137,31 +133,40 @@ export default function Example() {
             ))}
           </ul>
         </div>
-        <div className="mt-16 w-full  flex-col flex">
+        <div className="mt-10 w-full  flex-col flex">
           <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Design
           </p>
           <ul
             role="list"
-            className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8"
+            className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8 mt-8"
           >
             {design.map((file) => (
-              <li key={file.title} className="relative">
-                <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-                  {/* <img
+              <li
+                key={file.title}
+                className="relative flex flex-col justify-center items-center"
+              >
+                {/* <div className="group aspect-h-7 aspect-w-10 block w-full rounded-lg">
+                  <img
                     src={file.image}
                     alt=""
                     className="pointer-events-none object-cover group-hover:opacity-75"
-                  /> */}
-                  {/* <button
+                  /> 
+                   <button
                     type="button"
                     className="absolute inset-0 focus:outline-none"
                   >
                     <span className="sr-only">
                       View details for {file.title}
                     </span>
-                  </button> */}
+                  </button>
                 </div>
+                <FontAwesomeIcon icon={faFigma} size="3x" /> */}
+                <FontAwesomeIcon
+                  icon={[file.prefix, file.icon]}
+                  size="3x"
+                  color="white"
+                />
                 <a
                   href={file.link}
                   download={file.title}
